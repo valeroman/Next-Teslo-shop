88af2b5b6d8b7998695670a1a283bf5a21543d69
Roman Valero <valeroman@gmail.com>
2022-07-19 15:54:42 -0500

    Conexion a BD y api

---
+++ database/db.ts
@@ -0,0 +1,45 @@
+import mongoose from 'mongoose';
+
+/** 
+ * 0 = disconnected
+ * 1 = connected
+ * 2 = connecting
+ * 3 = disconnecting
+*/
+
+const mongooseConnection = {
+    isConnected: 0
+}
+
+export const connect = async() => {
+
+    if ( mongooseConnection.isConnected ) {
+        console.log('Ya estabamos conectado');
+        return;
+    }
+
+    if ( mongoose.connections.length > 0 ) {
+        mongooseConnection.isConnected = mongoose.connections[0].readyState;
+
+        if ( mongooseConnection.isConnected === 1 ) {
+            console.log('Usando conexión anterios');
+            return;
+        }
+
+        await mongoose.disconnect();
+    }
+
+    await mongoose.connect( process.env.MONGO_URL || '' );
+    mongooseConnection.isConnected = 1;
+    console.log('Conectado a MongoDB:',  process.env.MONGO_URL );
+}
+
+export const disconnect = async() => {
+
+    if ( process.env.NODE_ENV === 'development' ) return;
+    
+    if ( mongooseConnection.isConnected === 0 ) return;
+
+    await mongoose.disconnect();
+    console.log('Desconectado de MongoDB')
+}
---
+++ pages/api/products/[slug].ts
@@ -0,0 +1,39 @@
+import type { NextApiRequest, NextApiResponse } from 'next';
+import { db } from '../../../database';
+import { IProduct } from '../../../interfaces';
+import Product from '../../../models/Product';
+
+type Data = 
+| { message: string }
+| IProduct
+
+export default function handler(req: NextApiRequest, res: NextApiResponse<Data>) {
+
+
+    switch (req.method) {
+        case 'GET':
+            return getProductBySlug( req, res );
+    
+        default:
+            return res.status(400).json({
+                message: 'Bad request'
+            });
+    }
+}
+
+const getProductBySlug = async(req: NextApiRequest, res: NextApiResponse<Data>) => {
+
+    const { slug } = req.query;
+
+    await db.connect();
+    const product = await Product.findOne({ slug }).lean();  
+    await db.disconnect();
+
+    if ( !product ) {
+        return res.status(404).json({
+            message: 'Producto no encontrado'
+        })
+    }
+
+    return res.json( product );
+}
