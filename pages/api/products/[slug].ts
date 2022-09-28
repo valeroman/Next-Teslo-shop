70607cc3e3eeaee6aaf57501592d715450b5889e
Roman Valero <valeroman@gmail.com>
2022-09-28 07:52:27 -0500

    Mantenimiento de productos

--- pages/api/products/[slug].ts
+++ pages/api/products/[slug].ts
@@ -34,5 +34,11 @@ const getProductBySlug = async(req: NextApiRequest, res: NextApiResponse<Data>) 
         })
     }
 
+    product.images = product.images.map( image => {
+        return image.includes('http') ? image : `${ process.env.HOST_NAME }products/${ image }`
+    });
+
+
+
     return res.json( product );
 }
